3956ec219cab30c43c7dd15b50f81ce4d5583724
DonnieDarko8881 <44477859+DonnieDarko8881@users.noreply.github.com>
2019-01-08 23:34:45 +0100

    Update MailService.java

--- kodilla-good-pattern/src/main/java/com/kodilla/good/patterns/challanges/FOOD2Order/informationService/MailService.java
+++ kodilla-good-pattern/src/main/java/com/kodilla/good/patterns/challanges/FOOD2Order/informationService/MailService.java
@@ -6,32 +6,32 @@ public class MailService implements InformationService {
 
     @Override
     public void informConfirmationOfRealisation(User user) {
-        System.out.println("Dear " + user.getName() + " " + user.getSurname()+"!");
+        System.out.println("Dear " + user.getName() + " " + user.getSurname() + "!");
         System.out.println("Your Order was realized");
     }
 
     @Override
     public void informRejectionOfRealisation(User user) {
-        System.out.println("Dear " + user.getName() + " " + user.getSurname()+"!");
+        System.out.println("Dear " + user.getName() + " " + user.getSurname() + "!");
         System.out.println("Your Order was rejected");
     }
 
     @Override
     public void informClaimOrder(User user) {
-        System.out.println("Dear " + user.getName() + " " + user.getSurname()+"!");
+        System.out.println("Dear " + user.getName() + " " + user.getSurname() + "!");
         System.out.println("Your Order was claimed");
     }
 
     @Override
     public void informFoodPrepared(User user) {
-        System.out.println("Dear " + user.getName() + " " + user.getSurname()+"!");
+        System.out.println("Dear " + user.getName() + " " + user.getSurname() + "!");
         System.out.println("Your Food was prepared");
     }
 
     @Override
     public void informDeliverOrdered(User user) {
-        System.out.println("Dear " + user.getName() + " " + user.getSurname()+"!");
-        System.out.println("Deliver is driving to you ");
+        System.out.println("Dear " + user.getName() + " " + user.getSurname() + "!");
+        System.out.println("Deliver is driving to you");
     }
 
 }
