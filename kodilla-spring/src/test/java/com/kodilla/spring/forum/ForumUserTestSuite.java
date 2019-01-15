81f8bfaeb25abdc2e56ebbf3b7e252ac50cd9b10
DonnieDarko8881 <44477859+DonnieDarko8881@users.noreply.github.com>
2019-01-15 14:30:45 +0100

    Update ForumUserTestSuite.java

--- kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -12,6 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ForumUserTestSuite {
 
+ 
     @Test
     public void GetUsernameJohnSmith(){
         //Given
